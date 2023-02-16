02701bd1eb405b2f769249f57f5629d5221942e8
Robert Balcerowicz <balcerowicz87@gmail.com>
2023-02-16 07:56:13 +0100

    Add shortcut to base config

--- src/app/dialogs/Tools.tsx
+++ src/app/dialogs/Tools.tsx
@@ -1,13 +1,16 @@
-import Dialog from "../../components/Dialog";
-import Outset from "../../components/Outset";
 import Button from "../../components/Button";
+import Dialog from "../../components/Dialog";
 import Input from "../../components/Input";
-import OutsetHead from "../../components/OutsetHead";
-import { message } from "@tauri-apps/api/dialog";
-import { invoke } from "@tauri-apps/api";
-import { useState } from "preact/hooks";
 import Inset from "../../components/Inset";
+import Outset from "../../components/Outset";
+import OutsetHead from "../../components/OutsetHead";
 import { useRunner } from "../contexts/runnerContext";
+
+import { resolveResource } from "@tauri-apps/api/path";
+import { invoke } from "@tauri-apps/api";
+import { message } from "@tauri-apps/api/dialog";
+import { open } from "@tauri-apps/api/shell";
+import { useState } from "preact/hooks";
 
 type ToolsProps = {
   show: boolean;
@@ -22,7 +25,7 @@ const Tools = (props: ToolsProps) => {
   return (
     <Dialog show={props.show} onHide={props.onHide}>
       <div style="width: 540px; display: flex; flex-direction: column;">
-        <Outset style="flex: 1 1 auto; display: flex; flex-direction: column; gap: 8px;">
+        <Outset style="flex: 0 0 auto; display: flex; flex-direction: column; gap: 8px;">
           <OutsetHead>DOSBox runner</OutsetHead>
           <Input
             id="arguments"
@@ -32,28 +35,47 @@ const Tools = (props: ToolsProps) => {
             onChange={(event) => {
               if (event.target instanceof HTMLInputElement) setParams(event.target.value);
             }}
-          />
-          <Button
-            disabled={running}
-            onClick={async () => {
-              if (running) return;
+            after={
+              <Button
+                disabled={running}
+                onClick={async () => {
+                  if (running) return;
 
-              try {
-                setRunning(true);
-                const runOutput = await invoke<string>("run_dosbox", { params });
-                setLastRunOutput(runOutput);
-              } catch (err: unknown) {
-                message(String(err), { type: "error" });
-              } finally {
-                setRunning(false);
-              }
-            }}
-          >
-            Run DOSBox
-          </Button>
+                  try {
+                    setRunning(true);
+                    const runOutput = await invoke<string>("run_dosbox", { params });
+                    setLastRunOutput(runOutput);
+                  } catch (err: unknown) {
+                    message(String(err), { type: "error" });
+                  } finally {
+                    setRunning(false);
+                  }
+                }}
+              >
+                Run DOSBox
+              </Button>
+            }
+          />
           <div style="display: flex; flex-direction: column; gap: 4px;">
             <div>Last run output</div>
             <Inset style="height: 240px;">{lastRunOutput}</Inset>
+          </div>
+        </Outset>
+        <Outset style="flex: 1 1 auto; display: flex; flex-direction: column; gap: 8px;">
+          <OutsetHead>Miscellaneous</OutsetHead>
+          <div style="flex: 0 0 auto; display: flex; justify-content: flex-end; gap: 2px;">
+            <Button
+              onClick={async () => {
+                try {
+                  const path = await resolveResource("base.conf");
+                  await open(path);
+                } catch (err: unknown) {
+                  message(String(err), { type: "error" });
+                }
+              }}
+            >
+              Open base config
+            </Button>
           </div>
         </Outset>
         <Outset style="flex: 0 0 auto; display: flex; justify-content: flex-end; gap: 2px;">
